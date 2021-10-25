c303f627ebe81f132660c3e5f44e3046678fc735
Ruben Cardenas <rubencg88@gmail.com>
2021-10-24 21:19:47 -0500

    Fetching users from AWS API

--- src/js/components/LearnerBtn.js
+++ src/js/components/LearnerBtn.js
@@ -1,30 +1,42 @@
-import React, { useState } from 'react';
-import brownie from '../../img/brownie.jpg';
-import zelda from '../../img/zelda.jpg';
-import parents from '../../img/parents.jpg';
+import React, { useState, useEffect } from 'react';
 import { Row, Col, Image } from 'react-bootstrap';
+import { API } from 'aws-amplify';
+
+function getUsersData() {
+    const apiName = 'fluentapi';
+    const path = '/users';
+    const myInit = {
+        headers: {},
+    };
+    return API.get(apiName, path, myInit);
+}
 
 function LearnerBtn(props) {
+    const [loading, setLoading] = useState(true);
+    const [users, setUsers] = useState([]);
 
-  return (
-    <Row>
-        <Col className="text-center">
-            <button className="learner-btn" >
-                <Image src={brownie} rounded />
-            </button>
-        </Col>
-        <Col className="text-center">
-            <button className="learner-btn" >
-                <Image src={zelda} rounded />
-            </button>
-        </Col>
-        <Col className="text-center">
-            <button className="learner-btn" >
-                <Image src={parents} rounded />
-            </button>
-        </Col>
-    </Row>
-  );
+    useEffect(() => {
+        getUsersData().then(response => {
+            setLoading(false);
+            if (response.data) {
+                setUsers(response.data);
+            }
+        });
+    }, []);
+
+    return loading ? <></> : (
+        <Row>
+            {users.map(user => {
+                const logo = `${process.env.PUBLIC_URL}/images/${user.image}`;
+                console.log("logo", logo);
+                return <Col className="text-center" key={user.name}>
+                    <button className="learner-btn" >
+                        <Image src={logo} rounded />
+                    </button>
+                </Col>
+            })}
+        </Row>
+    );
 }
 
 export default LearnerBtn;
